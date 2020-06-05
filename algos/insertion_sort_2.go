a81a5b1ae286bdcf5e25af8aa4f2da7ec32427a8
tomaslingotti <tomaslingotti@gmail.com>
2020-06-05 19:32:25 -0300

    add insertion sort

---
+++ algos/insertion_sort_2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{2, 4, 5, 7, 3, 8}
+	fmt.Println(insertion(arr))
+}
+
+func insertion(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		for j := 0; j < i; j++ {
+			if arr[j] > arr[i] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	return arr
+}
