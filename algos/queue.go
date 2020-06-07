ea3942a1d8264b6ea2cb6e00c45b5851f5685a05
tomaslingotti <tomaslingotti@gmail.com>
2020-06-07 13:51:21 -0300

    queue implementation

---
+++ algos/queue.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type Queue struct {
+	nodes []NodeQ
+}
+
+func main() {
+	q := makeQueue()
+
+	q.add(1)
+	q.add(2)
+	q.add(3)
+
+	fmt.Println(q.size())
+
+	node :=	q.remove()
+
+	fmt.Println(node.data)
+	fmt.Println(q.size())
+}
+
+func makeQueue() Queue {
+	return Queue{
+		nodes: []NodeQ{},
+	}
+}
+
+type NodeQ struct {
+	data int
+}
+
+func makeNodeQ(data int) NodeQ {
+	return NodeQ{data: data}
+}
+
+func (q *Queue) add(data int) bool {
+	node := makeNodeQ(data)
+	q.nodes = append(q.nodes, node)
+	return true
+}
+
+func (q *Queue) remove() *NodeQ {
+	if len(q.nodes) == 0 {
+		return nil
+	}
+	dequeue := q.nodes[0] //FIFO
+	q.nodes = q.nodes[1:]
+	return &dequeue
+}
+
+func (q *Queue) size() int {
+	return len(q.nodes)
+}
---
+++ algos/stack.go
@@ -0,0 +1,11 @@
+package main
+
+func peek() {
+	/*
+		l := len(q.nodes)
+		if l == 0 {
+			return nil
+		}
+		return &q.nodes[l-1 : l][0]
+	 */
+}
