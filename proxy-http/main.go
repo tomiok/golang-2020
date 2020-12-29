1683382f29dae8dcca9e646d93080d7bf5aa068d
tomas lingotti <tomaslingotti@gmail.com>
2020-12-29 04:01:59 -0300

    proxy working

--- proxy-http/main.go
+++ proxy-http/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -35,29 +37,39 @@ func connect() {
 func proxy(conn net.Conn) {
 	defer conn.Close()
 
-	remoteConnection, err := net.Dial("tcp", "google.com:443")
+	remoteConnection, err := net.Dial("tcp", "localhost:3001")
 
 	if err != nil {
 		log.Println(err.Error())
 		return
 	}
+	remoteConnection.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	remoteConnection.SetReadDeadline(time.Now().Add(10 * time.Second))
 	defer remoteConnection.Close()
 
-	go io.Copy(remoteConnection, conn)
-	io.Copy(conn, remoteConnection)
+	go func() {
+		io.Copy(remoteConnection, conn)
+	}()
+
+	var buff [150]byte
+	n, _ := remoteConnection.Read(buff[:])
+	s := string(buff[:n])
+	rdr := strings.NewReader(s)
+	fmt.Println(s)
+	io.Copy(conn, rdr)
 }
 
 func copyWhenErr(conn net.Conn) {
 	defer conn.Close()
-	 for {
-	 	var buf [128]byte
-	 	n, err := conn.Read(buf[:])
+	for {
+		var buf [128]byte
+		n, err := conn.Read(buf[:])
 
-	 	if err != nil {
-	 		log.Println(err.Error())
+		if err != nil {
+			log.Println(err.Error())
 			return
 		}
-		 _ = conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-		 os.Stderr.Write(buf[:n])
-	 }
+		_ = conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		os.Stderr.Write(buf[:n])
+	}
 }
